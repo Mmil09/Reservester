320c0896c4e22125a4227b7b23ada7a5318af8c3
Mmil09 <Mmil09@gmail.com>
2014-07-08 16:19:58 -0400

    deleted unused models and controllers

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-	validates_uniqueness_of :email_address
-	has_many :restaurants
-
-	scope :alphabetical, -> {where('first_name ASC')} 
-	scope :admin, -> {where(admin: true)} #passes where into the scope function
-end
