c4a7fd974e12b78a401c570b1d2031ef795046f5
Mmil09 <Mmil09@gmail.com>
2014-07-09 12:28:17 -0400

    fixed permit method in restaurant_param in restaurant controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,5 @@
 class Restaurant < ActiveRecord::Base
 	validates_uniqueness_of :name
-	validates_presence_of :phone_number
-	belongs_to :user
-
-	before_validation do
-		self.phone_number = "1-555-555-5555"
-	end
+	# validates_presence_of :phone_number
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707194608) do
+ActiveRecord::Schema.define(version: 20140708205619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,8 @@ ActiveRecord::Schema.define(version: 20140707194608) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "phone_number"
-    t.integer  "user_id"
+    t.text     "full_address"
+    t.text     "description"
   end
 
   create_table "users", force: true do |t|
