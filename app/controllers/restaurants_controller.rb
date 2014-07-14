094762f85b15d9f8677fccf216798978c7a562a7
Mmil09 <Mmil09@gmail.com>
2014-07-14 12:52:58 -0400

    tried to fix

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -82,4 +82,5 @@ class RestaurantsController < ApplicationController
     end
 
   end
+end
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ActiveRecord::Base
 	validates_uniqueness_of :name
 	# validates_presence_of :phone_number
 	mount_uploader :image, ImageUploader
+	
 end
